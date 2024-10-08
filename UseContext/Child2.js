ed448eb5c89b791e8da9c799610df49ca9895a5e
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-10-08 14:40:09 +0530

    UseContext

---
+++ UseContext/Child2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Child3 from './Child3';
+function Child2(){
+    return(
+        <>
+        <p>Child2: This is second child component</p>
+        <Child3/>
+        </>
+    );
+}
+export default Child2;
