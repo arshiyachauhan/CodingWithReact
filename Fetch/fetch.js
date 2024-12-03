af6daa0150c3ef13d43d92ac60fd882e02a3fd42
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-12-03 11:26:28 +0530

    UseEffect for fetching data

---
+++ Fetch/fetch.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const FetchData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setData(data.slice(0, 10))); // Limit to 10 items
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from API</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FetchData;
